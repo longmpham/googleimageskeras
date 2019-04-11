374cf1a55f2f3bac659d773700c459f8f5889008
Long <longish222@gmail.com>
2019-04-11 10:06:45 -0400

    initial commit with google opening file sort of started

---
+++ Code/google_image_downloader.py
@@ -0,0 +1,14 @@
+###############
+#
+# Created by Long Pham
+#
+# This python script allows the user to find images from Google, 
+# parse the URL and then download the image from a simple query
+# and save it to a drive. The user will be able to select the simple query
+# like "Dog" in Google Search to produce the dataset. 
+# I'm hoping that I will be able to select up to 100 or more images
+#
+###############
+
+
+
