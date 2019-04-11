51f3eb596ce68c3893f3e737e8f0ea166631940d
Long <longish222@gmail.com>
2019-04-11 16:45:30 -0400

    builds but doesnt train

--- Code/google_image_downloader.py
+++ Code/google_image_downloader.py
@@ -37,12 +37,11 @@ from google_images_download import google_images_download
 
 # Keras
 from keras.models import Sequential
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Flatten
-from keras.layers.core import Dense
+from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Dense, Activation, Dropout, Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import sgd
 from keras import backend as K
+import keras
 
 # OpenCV (Image Pre-processing)
 import cv2
@@ -53,6 +52,7 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 
 
@@ -63,7 +63,19 @@ image_dir = './downloads'
 categories = ['dog','cat']
 IMG_SIZE = 128
 
-
+init_method = 'glorot_uniform'
+input_shape = (128,128,1)
+first_layer_filter_size = 96
+dropout = 0.5
+num_classes = 2 
+learning_rate = 0.01
+momentum = 0.9
+learning_rate_decay = 0.0005
+batch_size = 32
+validation_split = 10
+loss_type = 'categorical_crossentropy'
+epochs = 10
+num_tests = 2
 
 ################## CREATE DATABASE ##################
 #TODO: ALLOW USER TO INPUT DATA OR NOT. IF SO, WHAT TYPE? FILL IN KEYWORDS
@@ -82,50 +94,39 @@ def get_images():
 		# print(paths)   #printing absolute paths of the downloaded images
 
 
-################## AUGMENT IMAGES ##################
+################## PRE-PROCESS IMAGES ##################
 #TODO: ALLOW USER TO AUGMENT OR NOT. (RECOMMENDED IF <100 IMAGES)
+def create_training_set():
 
-training_data = []
-# def normalize_data():
-for category in categories:
-	path = os.path.join(image_dir, category)
-	class_num = categories.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
-	for img in os.listdir(path):
-		img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
-		new_img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-		training_data.append([new_img_array, class_num])
-		print(len(training_data))
+	training_data = []
+	for category in categories:
+		path = os.path.join(image_dir, category)
+		class_num = categories.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
+		for img in os.listdir(path):
+			img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+			new_img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+			training_data.append([new_img_array, class_num])
 
-		#plt.imshow(new_img_array, cmap='gray')
-		#plt.show()
-inputd=input()
-exit()
+			#plt.imshow(new_img_array, cmap='gray')
+			#plt.show()
+	# print(training_data)
+	# print(len(training_data))
+	random.shuffle(training_data)
+
+	X = [] # create image data
+	y = [] # label
+
+	for features, label in training_data:
+		X.append(features)
+		y.append(label)
+	X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+	X = X/255.0
+
+	y = keras.utils.to_categorical(y, num_classes)
+
+	return(X,y)
 
 ################## KERAS ENVIRONMENT ##################
-
-# Load dataset
-def load_data(num_classes):
-    # load data
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
-    # reshape dataset to feed it into model properly
-    x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-    x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-    
-    # normalize train/test data
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255.0
-    x_test /= 255.0
-    
-    # convert class vectors to matrices as binary
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
-    
-    print('Number of train samples in MNIST: ', x_train.shape[0])
-    print('Number of test samples in MNIST: ', x_test.shape[0])
-    
-    return (x_train, y_train), (x_test, y_test)
 
 # Build AlexNet-like model
 def build_model(init_method, input_shape, first_layer_filter_size, dropout, num_classes):
@@ -190,7 +191,39 @@ def build_model(init_method, input_shape, first_layer_filter_size, dropout, num_
 
     return model
 
+def main():
+
+	# Load Images
+	print('Creating Dataset...')
+	images, labels = create_training_set()
+
+	# Create Keras Model
+	print('Building model(s)...')
+	model = build_model(init_method, input_shape, first_layer_filter_size, dropout, num_classes)
+	
+	# Compile uniform model
+	print('Compiling model(s)...')
+	optimizer = sgd(learning_rate, momentum, learning_rate_decay)
+	model.compile(optimizer=optimizer, loss=loss_type, metrics=['accuracy'])
 
 
-# def main():
-# 	normalize_data()
+	for i in range(num_tests):
+		try:
+			print('Training model(s)...')
+			history = model.fit(images,labels,
+					batch_size=batch_size,
+					epochs=epochs,
+					validation_split=validation_split,
+					shuffle=True)
+
+			# Evaluate Model
+			print('Evaluating model(s)...')
+			# score = model.evaluate(x_test, y_test)
+
+
+		except KeyboardInterrupt:
+			print('user input to end')
+			K.clear_session()
+
+if __name__ == "__main__":
+	main()	
